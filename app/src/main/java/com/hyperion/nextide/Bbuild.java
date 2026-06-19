4fe48fd5d470ea356a42426c006d79ce5bb86761
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-06-20 00:07:11 +0700

    Update Bbuild.java

--- app/src/main/java/com/hyperion/nextide/Bbuild.java
+++ app/src/main/java/com/hyperion/nextide/Bbuild.java
@@ -10,18 +10,22 @@ public class Bbuild {
     private String ecjPath;
     private String d8Path;
     private String apkSignerPath;
-    private String javacPath;
+    private String aapt2Path;
     private String zipalignPath;
 
     public Bbuild(Context context) {
         this.context = context;
-        // Android 12 အကြိုက် Internal Storage လမ်းကြောင်း သတ်မှတ်ခြင်း
+        
+        // ၁။ Assets ထဲက .jar ဖိုင်များအတွက် Internal Storage လမ်းကြောင်း
         this.toolsDir = context.getFilesDir().getAbsolutePath() + "/tools/";
         this.ecjPath = toolsDir + "ecj.jar";
         this.d8Path = toolsDir + "d8.jar";
         this.apkSignerPath = toolsDir + "apksigner.jar";
-        this.javacPath = toolsDir + "javac";
-        this.zipalignPath = toolsDir + "zipalign";
+        
+        // ၂။ jniLibs/arm64-v8a ထဲက .so ဖိုင်များအတွက် Native Library လမ်းကြောင်း (Android 12+ အကြိုက်)
+        String nativeDir = context.getApplicationInfo().nativeLibraryDir;
+        this.aapt2Path = nativeDir + "/libaapt2.so";
+        this.zipalignPath = nativeDir + "/libzipalign.so";
     }
 
     /**
@@ -29,7 +33,6 @@ public class Bbuild {
      */
     public void compileJava(String sourceFolder, String outputBinFolder) {
         try {
-            // ဥပမာ - ecj.jar သို့မဟုတ် javac ကို သုံးပြီး run မည့် command
             String command = "dalvikvm -cp " + ecjPath + " org.eclipse.jdt.internal.compiler.batch.Main " 
                            + sourceFolder + " -d " + outputBinFolder;
                            
@@ -45,7 +48,10 @@ public class Bbuild {
      */
     public void convertToDex(String binFolder, String outputDexFolder) {
         try {
-            String command = "dalvikvm -jar " + d8Path + " --output " + outputDexFolder + " " + binFolder + "/*.class";
+            // d8.jar ကို com.android.tools.r8.D8 Main Class နဲ့ တွဲပြီး run ပေးရပါမယ်
+            String command = "dalvikvm -cp " + d8Path + " com.android.tools.r8.D8 --output " 
+                           + outputDexFolder + " " + binFolder + "/*.class";
+                           
             Process process = Runtime.getRuntime().exec(command);
             process.waitFor();
         } catch (Exception e) {
